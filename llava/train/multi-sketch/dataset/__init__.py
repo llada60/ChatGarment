20b33f8aa6f673ed61e47fbd49e11cc5026899f4
Ling Liu <572192783@qq.com>
2025-07-21 17:38:57 +0200

    update multi sketch

--- llava/train/multi-sketch/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-from .loader import LazySupervisedDataset, LazySupervisedDatasetCmb
-from .collator import DataCollatorForSupervisedDataset
-
-__all__=[
-    "LazySupervisedDataset",
-    "LazySupervisedDatasetCmb",
-    "DataCollatorForSupervisedDataset"
-]
