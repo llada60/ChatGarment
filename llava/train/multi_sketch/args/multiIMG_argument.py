9b6f9842dd15ec9baa96e68b7f05bd10d9b6b71f
Ling Liu <572192783@qq.com>
2025-07-25 14:03:23 +0200

    fix bug

--- llava/train/multi_sketch/args/multiIMG_argument.py
+++ llava/train/multi_sketch/args/multiIMG_argument.py
@@ -18,6 +18,7 @@ class ModelArguments:
     mm_use_im_patch_token: bool = field(default=True)
     mm_patch_merge_type: Optional[str] = field(default='flat')
     mm_vision_select_feature: Optional[str] = field(default="patch")
+    mm_resampler_type: Optional[str] = field(default="spatial_pool")
 
 
 @dataclass
