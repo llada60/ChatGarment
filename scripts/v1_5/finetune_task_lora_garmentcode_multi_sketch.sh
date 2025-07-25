d02688dd3656b4788b90fa83e2c41ecfef2b69f4
Ling Liu <572192783@qq.com>
2025-07-25 14:36:41 +0200

    update sh

--- scripts/v1_5/finetune_task_lora_garmentcode_multi_sketch.sh
+++ scripts/v1_5/finetune_task_lora_garmentcode_multi_sketch.sh
@@ -26,10 +26,11 @@ deepspeed llava/train/multi_sketch/train_mem_garmentcode_outfit.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
+    --mm_resampler_type "spatial_pool" \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
-    --bf16 False \
-    --output_dir ./checkpoints/qwen2-7b-ov \
+    --bf16 True \
+    --output_dir ./checkpoints/llava-v1.5-7b-task-lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -43,11 +44,10 @@ deepspeed llava/train/multi_sketch/train_mem_garmentcode_outfit.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 False \
+    --tf32 True \
     --model_max_length 3072 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
-
+    --report_to wandb 
 
